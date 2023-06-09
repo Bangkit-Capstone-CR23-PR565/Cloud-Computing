d2d9e5557680b8b48d70502cf26bcd5166fbedf9
aljauzr <aljauzir@gmail.com>
2023-06-09 15:59:31 +0700

    changed db schema

--- database/capstone_eventmu.sql
+++ database/capstone_eventmu.sql
@@ -1,17 +1,17 @@
 CREATE TABLE `events` (
   `id` int(11) NOT NULL,
-  `name` text DEFAULT NULL,
-  `category` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `image_url` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `description` text DEFAULT NULL,
+  `name` text,
+  `category` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `image_url` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `description` text,
   `stand_available` int(3) DEFAULT NULL,
   `price_per_stand` int(11) DEFAULT NULL,
   `like_count` int(11) DEFAULT NULL,
-  `location` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `location` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `start_date` date DEFAULT NULL,
   `end_date` date DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp()
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `rating` (
@@ -19,15 +19,9 @@ CREATE TABLE `rating` (
   `user_id` int(11) DEFAULT NULL,
   `event_id` int(11) DEFAULT NULL,
   `user_rating` int(2) DEFAULT NULL,
-  `user_comment` text DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `recommendation_result` (
-  `user_id` int(11) DEFAULT NULL,
-  `event_id` int(11) DEFAULT NULL,
-  `added_at` datetime DEFAULT NULL
+  `user_comment` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
@@ -37,13 +31,14 @@ CREATE TABLE `users` (
   `password` text NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
-  `category_interest` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `refresh_token` text DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+  `category_interest` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `refresh_token` text,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `user_likes` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `event_id` int(11) DEFAULT NULL,
   `added_at` datetime DEFAULT NULL
@@ -57,35 +52,31 @@ ALTER TABLE `rating`
   ADD KEY `user_id` (`user_id`),
   ADD KEY `event_id` (`event_id`);
 
-ALTER TABLE `recommendation_result`
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `event_id` (`event_id`);
-
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `phone` (`phone`),
   ADD UNIQUE KEY `email` (`email`(30));
 
 ALTER TABLE `user_likes`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `event_id` (`event_id`);
 
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=401;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=411;
 
 ALTER TABLE `rating`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+ALTER TABLE `user_likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 ALTER TABLE `rating`
   ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
-
-ALTER TABLE `recommendation_result`
-  ADD CONSTRAINT `recommendation_result_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `recommendation_result_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
 
 ALTER TABLE `user_likes`
   ADD CONSTRAINT `user_likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
